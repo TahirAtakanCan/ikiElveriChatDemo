a1f1e90b043b9a80b70706ce2566b3dc944c8ad7
TahirAtakanCan <atkan4202@gmail.com>
2024-06-06 14:14:51 +0300

    feat: YPImagePicker setup and FileStorage begin

--- ikiElveriChatDemo/EditProfileTableViewController.swift
+++ ikiElveriChatDemo/EditProfileTableViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import YPImagePicker
+import Toast
+
 
 class EditProfileTableViewController: UITableViewController {
     
@@ -15,12 +18,15 @@ class EditProfileTableViewController: UITableViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var statusLabel: UILabel!
     
+    //MARK: - Vars
+    //var uploadAvatarImage: YPImagePicker!
+    
     //MARK: - ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.tableFooterView = UIView()
-
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -47,6 +53,13 @@ class EditProfileTableViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    
+    //MARK: - IBActions
+    
+    @IBAction func editButtonPressed(_ sender: Any) {
+        showImagePicker()
+    }
+    
     //MARK: - UpdateUI
     private func showUserInfo(){
         
@@ -67,8 +80,34 @@ class EditProfileTableViewController: UITableViewController {
         usernameTextField.delegate = self
         usernameTextField.clearButtonMode = .whileEditing
     }
+    
+    //MARK: - YPImagePicker
+    func showImagePicker() {
+        var config = YPImagePickerConfiguration()
+        config.screens = [.library, .photo]
+        config.library.mediaType = .photo
+        
+        let picker = YPImagePicker(configuration: config)
+        picker.didFinishPicking { [unowned picker] items, _ in
+            if let photo = items.singlePhoto {
+                self.uploadAvatarImage(photo.image)
+                self.avatarImageView.image = photo.image.circleMasked
+            } else {
+                //ProgressHUD.showError("Couldn't select image!")
+            }
+            picker.dismiss(animated: true, completion: nil)
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    private func uploadAvatarImage(_ image: UIImage) {
+            // Avatar yükleme işlemleri burada yapılacak
+            // Örneğin, Firebase'e yükleme kodu burada olabilir
+            print("Uploading avatar image...")
+    }
+    
 }
-
 
 extension EditProfileTableViewController: UITextFieldDelegate {
     
@@ -90,3 +129,22 @@ extension EditProfileTableViewController: UITextFieldDelegate {
     }
     
 }
+
+extension UIImage {
+    var circleMasked: UIImage? {
+        let square = CGSize(width: min(size.width, size.height), height: min(size.width, size.height))
+        let imageView = UIImageView(frame: CGRect(origin: .zero, size: square))
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = self
+        imageView.layer.cornerRadius = square.width / 2
+        imageView.layer.masksToBounds = true
+        UIGraphicsBeginImageContextWithOptions(imageView.bounds.size, false, scale)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        imageView.layer.render(in: context)
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return result
+    }
+}
+
+
---
+++ ikiElveriChatDemo/FirebaseHelpers/FileStorage.swift
@@ -0,0 +1,9 @@
+//
+//  FileStorage.swift
+//  ikiElveriChatDemo
+//
+//  Created by Tahir Atakan Can on 6.06.2024.
+//
+
+import Foundation
+import Firebase
