765be8753a966234e4995582c4001ada9ff327a9
TahirAtakanCan <atkan4202@gmail.com>
2024-06-15 13:43:18 +0300

    feat: Mic button update

--- ikiElveriChatDemo/SecondaryView/ChatViewExtensions/InputBarAccessoryDelegate.swift
+++ ikiElveriChatDemo/SecondaryView/ChatViewExtensions/InputBarAccessoryDelegate.swift
@@ -14,6 +14,8 @@ extension ChatViewController : InputBarAccessoryViewDelegate {
         if text != "" {
             //print("typing...")
         }
+        
+        updateMicButtonStatus(show: text == "")
     }
     
     func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
